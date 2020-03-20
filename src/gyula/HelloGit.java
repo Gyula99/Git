f5a500fc86689cd95b4b6c7462cfb6fb3c4ea885
Gyula <99gyula99@gmail.com>
2020-03-20 09:25:40 +0100

    add new method

--- src/gyula/HelloGit.java
+++ src/gyula/HelloGit.java
@@ -1,7 +1,15 @@
 package gyula;
 
+
+
+
 public class HelloGit {
     public static void main(String[]args){
         System.out.println("Hello");
     }
+    public static void valami(){
+
+    }
+
+
 }
